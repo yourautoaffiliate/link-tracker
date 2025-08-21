e2664cd88653052515e9fe661dcfc863e852b008
Arpit goyal <arpitgoyal464@gmail.com>
2025-08-21 09:44:13 +0530

    added monitoring

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+const https = require('https');
 
 export function extractDomain(redirectUrl) {
   try {
@@ -30,20 +31,51 @@ export function extractDomain(redirectUrl) {
   }
 }
 
-export async function notifyAdmin(message) {
-  const BOT_TOKEN = '8332945325:AAEn4HDimUyNjk9SvHlQ9cglb_BAX6J0kwA';
-  const CHAT_ID = '572769491'; // This must be a user who has started your bot
-  // const MESSAGE = 'Hello from raw API!';
+// export async function notifyAdmin(message) {
+//   const BOT_TOKEN = '8332945325:AAEn4HDimUyNjk9SvHlQ9cglb_BAX6J0kwA';
+//   const CHAT_ID = '572769491'; // This must be a user who has started your bot
+//   // const MESSAGE = 'Hello from raw API!';
 
-  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
-  const body = {
+//   const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+//   const body = {
+//     chat_id: CHAT_ID,
+//     text: message,
+//   };
+//   await fetch(url, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(body),
+//   });
+//   // const data = await res.json();
+// }
+
+export async function notifyAdmin(message) {
+  const BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN';
+  const CHAT_ID = 'USER_CHAT_ID';
+  const MESSAGE = 'Hello from HTTPS!';
+
+  const data = JSON.stringify({
     chat_id: CHAT_ID,
     text: message,
-  };
-  await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body),
   });
-  // const data = await res.json();
+
+  const options = {
+    hostname: 'api.telegram.org',
+    path: `/bot${BOT_TOKEN}/sendMessage`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    let response = '';
+    res.on('data', (chunk) => (response += chunk));
+    res.on('end', () => console.log(response));
+  });
+
+  req.on('error', (e) => console.error(e));
+  req.write(data);
+  req.end();
 }
